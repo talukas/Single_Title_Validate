09ab0fb3768dac3d20617c6d85f6d3a3d6f33dcb
Tania <taluk@outlook.com>
2016-11-19 12:22:47 -0800

    First

---
+++ src/main/java/core/Single_Title_Validate/App.java
@@ -0,0 +1,13 @@
+package core.Single_Title_Validate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
